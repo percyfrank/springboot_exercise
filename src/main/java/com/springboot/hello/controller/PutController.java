deb1eb5083de325fcc663c8de5b2c01677d3ea5f
ohsuk kwon <percykwon@gmail.com>
2022-10-27 15:02:52 +0900

    [221027] ADD : PUT API 1. @RequestBody 활용

---
+++ src/main/java/com/springboot/hello/controller/PutController.java
@@ -0,0 +1,35 @@
+package com.springboot.hello.controller;
+
+import com.springboot.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+public class PutController {
+
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String,Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    // DTO 객체 활용
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto) {
+        return memberDto.toString();
+    }
+
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto) {
+        return memberDto;
+    }
+
+
+}
