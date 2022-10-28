5feada243bb624fcc0bb5b37bda1060a4c4e91b0
ohsuk kwon <percykwon@gmail.com>
2022-10-28 11:33:07 +0900

    [221028] ADD : Logback 적용

--- src/main/java/com/springboot/api/controller/GetController.java
+++ src/main/java/com/springboot/api/controller/GetController.java
@@ -3,25 +3,30 @@ package com.springboot.api.controller;
 import com.springboot.api.domain.dto.MemberDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public String getHello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName() {
+        log.info("getName으로 요청이 들어왔습니다.");
         return "ohsukKwon";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1으로 요청이 들어왔습니다.");
         return variable;
     }
 
