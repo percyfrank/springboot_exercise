a265b07fce721613f1e5ead97f3df7634f4e916d
ohsuk kwon <percykwon@gmail.com>
2022-10-28 14:13:41 +0900

    [221028] UPDATE : User, Userdao, UserController 1. add, finById, deleteAll 추가 2. Get, Delete Mapping

--- src/main/java/com/springboot/api/controller/UserController.java
+++ src/main/java/com/springboot/api/controller/UserController.java
@@ -1,17 +1,35 @@
 package com.springboot.api.controller;
 
 import com.springboot.api.dao.Userdao;
+import com.springboot.api.domain.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/user")
+@RequestMapping("/api/v1")
 public class UserController {
 
     private final Userdao userdao;
 
     public UserController(Userdao userdao) {
         this.userdao = userdao;
+    }
+
+    @GetMapping("/user")
+    public User add(User user) throws Exception {
+        userdao.add(new User("1", "kos", "1234"));
+        return userdao.findById("1");
+    }
+
+    @DeleteMapping("/user")
+    public ResponseEntity<Integer> deleteAll() {
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(userdao.deleteAll());
     }
 
 
