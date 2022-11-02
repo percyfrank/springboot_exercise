eb9bfb00e0c622f9892497ba2bf2ac1519f4bc2c
ohsuk kwon <percykwon@gmail.com>
2022-11-02 17:38:18 +0900

    [221102] UPDATE : HospitalController 수정

--- src/main/java/com/springboot/api/controller/HospitalController.java
+++ src/main/java/com/springboot/api/controller/HospitalController.java
@@ -3,10 +3,12 @@ package com.springboot.api.controller;
 import com.springboot.api.domain.Hospital;
 import com.springboot.api.dto.HospitalRequestDto;
 import com.springboot.api.repository.HospitalDao;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,9 +22,14 @@ public class HospitalController {
 
     @GetMapping(value = "/hospitals/{id}")
     public ResponseEntity<Hospital> get(@PathVariable int id) {
-        return ResponseEntity
-                .ok()
-                .body(hospitalDao.findById(id));
+        Hospital hospital = hospitalDao.findById(id);
+        Optional<Hospital> opt = Optional.of(hospital);
+
+        if(!opt.isEmpty()) {
+            return ResponseEntity.ok().body(hospital);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
+        }
     }
 
     @GetMapping(value = "/hospitals")
